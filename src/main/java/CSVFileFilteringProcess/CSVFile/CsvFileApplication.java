b32790782bbb7a4c4aaf2d32e503f9a81d95a592
mariem <mariem.saleh@hotmail.com>
2024-01-24 13:50:38 +0100

    Första commit: initierar projektet

---
+++ src/main/java/CSVFileFilteringProcess/CSVFile/CsvFileApplication.java
@@ -0,0 +1,13 @@
+package CSVFileFilteringProcess.CSVFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvFileApplication.class, args);
+	}
+
+}
